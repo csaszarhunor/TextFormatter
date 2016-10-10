487abfd0343251a93727c297587256626f5f4586
csaszarhunor <csaszarhunor@gmail.com>
2016-10-10 12:01:32 +0200

    split words too

--- src/hu/codecool/textFormatter/TextFormatter.java
+++ src/hu/codecool/textFormatter/TextFormatter.java
@@ -1,31 +1,40 @@
 package hu.codecool.textFormatter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextFormatter {
 
-	public String[] format(String raw, int charNum) {
-		String[] words = raw.split(" ");
-		String lines = "";
-		int charsLeft = charNum;
-		
-		for (int i = 0; i < words.length; i++) {
-			if (words[i].length() + 1 >= charsLeft){
-				lines += "/n" + words[i] + " ";
-				charsLeft = charNum - words[i].length() + 1;
+	public static String[] format(String text, int maxLineLength){
+		String[] words = text.split(" ");
+		List<String> lines = new ArrayList<>();
+		String line = "";
+		int freeChars = maxLineLength;
+		for (String word : words) {
+			if(word.length() > freeChars){
+				line += word.substring(0, freeChars);
+				lines.add(line);
+				
+				int splitIndex = freeChars;
+				while(word.substring(splitIndex).length() > maxLineLength){
+					lines.add(word.substring(splitIndex, splitIndex + maxLineLength));
+					splitIndex += maxLineLength;
+				}
+				line = word.substring(splitIndex) + " ";
+				freeChars = maxLineLength - line.length();
 			} else {
-				lines += words[i] + " ";
-				charsLeft -= words[i].length() + 1;
+				line += word + " ";
+				freeChars = maxLineLength - line.length();
 			}
 		}
-		
-		return lines.split("/n");
+		return (String[]) lines.toArray(new String[lines.size()]);
 	}
-
+	
 	public static void main(String[] args) {
-		String str = "12345 123 1234567 123456789 123 12 1 123 1234";
-		TextFormatter tf = new TextFormatter();
-		String[] result = tf.format(str, 4);
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+		String text = "I am a Codecooler dsdfs sdfsdfsf sdfsdffsfsddfsf sdf d d df dfdf sdf";
+		String[] lines = TextFormatter.format(text, 5);
+		for (String string : lines) {
+			System.out.println(string);
 		}
 	}
 }
